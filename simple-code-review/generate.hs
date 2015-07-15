0ba4a5ca7b282c6b417e32680013887f61e0236b
Michael Lee <michael.lee.0x2a@gmail.com>
2015-07-14 21:50:54 -0700

    Added more strict types

--- simple-code-review/generate.hs
+++ simple-code-review/generate.hs
@@ -5,62 +5,84 @@ import System.Exit
 import System.IO
 import Data.List
 import Data.List.Split
+import Data.String
 import Data.String.Utils
 import Data.Time
 import Data.Time.LocalTime
 import Data.Time.Format
 
 import Text.Format
-import Text.Pandoc
-
--- Basic config
-
-usageText :: String
-usageText = "generate\n\
-            \ \
-            \Usage:\n\
-            \  generate <lang> <title> <code> <feedback> <output>\n\
-            \ "
+import Text.Pandoc (readMarkdown, writeHtmlString, def)
 
 
 -- Data structures
 
 newtype LineNumber = LineNumber Integer deriving (Ord, Eq)
-instance Show LineNumber where
-    show (LineNumber n) = show n
+instance Show LineNumber where show (LineNumber n) = show n
+
+newtype SanitizedHtml = SanitizedHtml String deriving Eq
+instance Show SanitizedHtml where show (SanitizedHtml a) = a
+
+newtype Template = Template String
+--instance IsString Template where fromString = Template
+instance Show Template where show (Template a) = a
+
+newtype Language = Language String deriving Eq
+instance Show Language where show (Language a) = a
+
+newtype Title = Title String
+instance Show Title where show (Title a) = a
+
+newtype Timestamp = Timestamp String deriving (Ord, Eq)
+instance Show Timestamp where show (Timestamp a) = a
+
 
 data Feedback = 
     Feedback {
         fLineno :: LineNumber,
-        fText :: String
+        fText :: SanitizedHtml 
     } deriving (Show)
 
 data CodeFragment = 
     CodeFragment {
         cLineno :: LineNumber,
-        cText :: String
+        cText :: SanitizedHtml
     } deriving (Show)
 
 data Line = 
     Line {
         lineno :: LineNumber,
-        code :: String,
-        commentary :: String
+        code :: SanitizedHtml,
+        commentary :: SanitizedHtml
     } deriving (Show)
 
+data TemplateOptions = 
+    TemplateOptions {
+        template :: Template,
+        language :: Language,
+        title :: Title,
+        timestamp :: Timestamp
+    } deriving (Show)
+
+data SourceOptions = 
+    SourceOptions {
+        codePath :: FilePath,
+        feedbackPath :: FilePath,
+        outputPath :: FilePath
+    } deriving (Show)
 
 -- Parsing feedback file
 
-mungeCodeTags :: String -> String
-mungeCodeTags html = replace "\"><code>" "\">" t
+mungeCodeTags :: SanitizedHtml -> SanitizedHtml
+mungeCodeTags (SanitizedHtml html) = SanitizedHtml $ replace "\"><code>" "\">" temp
     where
-        t = replace "<pre class=\"" "<pre><code class=\"language-" html
+        temp = replace "<pre class=\"" "<pre><code class=\"language-" html
 
-convertToHtml :: String -> String
+convertToHtml :: String -> SanitizedHtml 
 convertToHtml text = 
     case readMarkdown def text of
-        Left error -> "error"
-        Right pandoc -> mungeCodeTags $ writeHtmlString def pandoc
+        Left error -> SanitizedHtml "error"
+        Right pandoc -> mungeCodeTags $ SanitizedHtml (writeHtmlString def pandoc)
 
 unindent :: String -> String
 unindent "" = ""
@@ -68,12 +90,12 @@ unindent text = drop 4 text
 
 parseSingle :: [String] -> Feedback
 parseSingle (rawLineNo : rest) = 
-    Feedback (LineNumber lineNo) html
+    Feedback lineNo html
     where
-        lineNo :: Integer
-        lineNo = read $ init rawLineNo
+        lineNo :: LineNumber
+        lineNo = LineNumber $ read (init rawLineNo)
 
-        html :: String
+        html :: SanitizedHtml
         html = convertToHtml $ intercalate "\n" $ map unindent rest 
 
 parseFeedback :: String -> [Feedback]
@@ -88,8 +110,8 @@ parseFeedback text = map (parseSingle . getLines) chunks
 
 -- Parse code
 
-escapeHtml :: String -> String
-escapeHtml html = intercalate "" $ map escape html
+escapeHtml :: String -> SanitizedHtml
+escapeHtml html = SanitizedHtml $ intercalate "" (map escape html)
     where
         escape :: Char -> String
         escape c = case c of
@@ -102,7 +124,7 @@ parseCode :: String -> [CodeFragment]
 parseCode text = map convertCode $ zip [1..] (splitOn "\n" text)
     where
         convertCode :: (Integer, String) -> CodeFragment
-        convertCode (index, "") = CodeFragment (LineNumber index) " "
+        convertCode (index, "") = CodeFragment (LineNumber index) (SanitizedHtml " ")
         convertCode (index, text) = CodeFragment (LineNumber index) (escapeHtml text)
 
 
@@ -112,7 +134,7 @@ makeLine :: CodeFragment -> Feedback -> Line
 makeLine code feedback = Line (cLineno code) (cText code) (fText feedback)
 
 makeSimpleLine :: CodeFragment -> Line
-makeSimpleLine code = Line (cLineno code) (cText code) ""
+makeSimpleLine code = Line (cLineno code) (cText code) (SanitizedHtml "")
 
 bundle :: [CodeFragment] -> [Feedback] -> [Line]
 bundle [] _ = []
@@ -123,8 +145,8 @@ bundle (c : cs) (f : fs)
 
 -- HTML conversion
 
-lineTemplate :: String 
-lineTemplate = 
+lineTemplate :: Template 
+lineTemplate = Template 
     "  <div class=\"line {0}\">\n\
     \    <div class=\"codeline\">\n\
     \      <div class=\"lineno\"><span>{1}</span></div>\n\
@@ -132,42 +154,66 @@ lineTemplate =
     \    </div>\n\
     \    <div class=\"commentary\">{4}</div>\n\
     \  </div>"
+
+formatTemplate :: Template -> [String] -> SanitizedHtml
+formatTemplate (Template template) fragments = 
+    SanitizedHtml $ format template fragments
    
-timestamp :: IO String
-timestamp = do 
+getTimestamp :: IO Timestamp
+getTimestamp = do 
     tz <- getCurrentTimeZone
     time <- getCurrentTime
-    return $ formatTime defaultTimeLocale formatStr (utcToZonedTime tz time)
+    let rawTime = formatTime defaultTimeLocale formatStr (utcToZonedTime tz time)
+    return $ Timestamp rawTime
     where 
+        formatStr :: String
         formatStr = "%A, %B %Y at %l:%M:%S %p, %Z"
 
-lineToHtml :: String -> Line -> String
-lineToHtml lang (Line lineNo code commentary) 
-    | commentary == "" = format lineTemplate ("not-annotated" : args)
-    | otherwise        = format lineTemplate ("annotated" : args)
+isAnnotated :: Line -> Bool
+isAnnotated line = (show (commentary line) /= "")
+
+lineToHtml :: Language -> Line -> SanitizedHtml
+lineToHtml (Language lang) line@(Line lineNo code commentary) 
+    | isAnnotated line = formatTemplate lineTemplate ("not-annotated" : args)
+    | otherwise        = formatTemplate lineTemplate ("annotated" : args)
     where 
         args :: [String]
-        args = [(show lineNo), lang, code, commentary]
+        args = [show lineNo, lang, show code, show commentary]
 
-linesToHtml :: String -> String -> String -> String -> [Line] -> String
-linesToHtml template title lang timestamp lines = 
-    format template [title, timestamp, intercalate "\n" (map (lineToHtml lang) lines)]
+linesToHtml :: Language -> [Line] -> SanitizedHtml
+linesToHtml lang lines = SanitizedHtml $ intercalate "\n" (map show htmlFragments)
+    where
+        htmlFragments :: [SanitizedHtml]
+        htmlFragments = map (lineToHtml lang) lines
+
+generateHtml :: TemplateOptions -> [Line] -> SanitizedHtml
+generateHtml (TemplateOptions template language title timestamp) lines =
+    formatTemplate template [show title, show timestamp, show innerHtml]
+    where 
+        innerHtml :: SanitizedHtml
+        innerHtml = linesToHtml language lines
 
 
 -- Commands
 
-generate :: String -> String -> FilePath -> FilePath -> FilePath -> IO ()
-generate lang title sourcePath feedbackPath outputPath = do
-    rawCode <- readFile sourcePath
-    rawFeedback <- readFile feedbackPath
-    template <- readFile "templates/feedback.html"
-    now <- timestamp
+generate :: TemplateOptions -> SourceOptions -> IO ()
+generate templateOptions sourceOptions = do
+    rawCode <- readFile (codePath sourceOptions)
+    rawFeedback <- readFile (feedbackPath sourceOptions)
 
     let code = parseCode rawCode
     let feedback = parseFeedback rawFeedback
     let lines = bundle code feedback
+    let output = generateHtml templateOptions lines
 
-    writeFile outputPath (linesToHtml template title lang now lines)
+    writeFile (outputPath sourceOptions) (show output)
+
+usageText :: String
+usageText = "generate\n\
+            \ \
+            \Usage:\n\
+            \  generate <lang> <title> <code> <feedback> <output>\n\
+            \ "
 
 usage :: IO ()
 usage = putStrLn usageText
@@ -178,9 +224,23 @@ exit = exitWith ExitSuccess
 
 -- Parsing and execution
 
+getTemplate :: FilePath -> IO Template
+getTemplate path = do
+    rawContent <- readFile path
+    return $ Template rawContent
+
 parse :: [String] -> IO ()
-parse [lang, title, source, feedback, output] = 
-        generate lang title source feedback output
+parse [language, title, source, feedback, output] = do
+    template <- getTemplate "templates/feedback.html"
+    timestamp <- getTimestamp
+
+    let language' = (Language language)
+    let title' = (Title title)
+
+    let templateOptions = (TemplateOptions template language' title' timestamp)
+    let sourceOptions = SourceOptions source feedback output
+
+    generate templateOptions sourceOptions
 parse _ = 
     die usageText
 
